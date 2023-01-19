deeb5a1cf3838378f1878607460303105256cdaf
Myron Law <myron@techelevator.com>
2023-01-19 14:33:27 -0500

    Added Mod1-3 lecture-final and Mod1-4 tutorial

---
+++ module-1/03_Logical_Branching/lecture-final/src/main/java/com/techelevator/MyMethodDemo.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+public class MyMethodDemo {
+
+    public static void main(String[] args) {
+        Lecture lecture = new Lecture();  // We will discuss this later
+
+        String ageCategory = "";
+        ageCategory = lecture.returnAdultOrMinorOrTeen(20);
+
+
+
+    }
+}
