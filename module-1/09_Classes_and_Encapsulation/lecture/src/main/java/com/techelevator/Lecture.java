890cea1d531e1b343f3f711e372b06ae41598642
Myron Law <myron@techelevator.com>
2023-01-27 08:30:07 -0500

    Initial push of Mod1-9 lecture material

---
+++ module-1/09_Classes_and_Encapsulation/lecture/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,12 @@
+package com.techelevator;
+
+import java.util.*;
+
+public class Lecture {
+
+    public static void main(String[] args) {
+        System.out.println("############################");
+        System.out.println("Classes and Encapsulation");
+        System.out.println("############################");
+    }
+}
