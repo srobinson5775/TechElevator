c070bc7807ca2fde63bacfd13512ffd5c8ff28cb
Myron Law <myron@techelevator.com>
2023-02-01 14:59:59 -0500

    added oop-bonus-lecture material

---
+++ bonus/oop-bonus-lecture/exercise/src/main/java/com/techelevator/BankAccount.java
@@ -0,0 +1,33 @@
+package com.techelevator;
+
+public class BankAccount {
+
+    public String accountHolderName;
+    public String accountNumber;
+    public int balance;
+
+    public BankAccount(String accountHolder, String accountNumber) {
+        this.accountHolderName = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public BankAccount(String accountHolder, String accountNumber, int balance) {
+        this.accountHolderName = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+
+    public int deposit(int amountToDeposit) {
+        balance = balance + amountToDeposit;
+        return balance;
+    }
+
+    public int withdraw(int amountToWithdraw) {
+        balance = balance - amountToWithdraw;
+        return balance;
+    }
+
+}
