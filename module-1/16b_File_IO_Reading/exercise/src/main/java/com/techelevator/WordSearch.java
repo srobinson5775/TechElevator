8ad2c46406b96a33c727f982d7687a0277a4ed46
Myron Law <myron@techelevator.com>
2023-02-06 11:38:59 -0500

    Added Mod1-16 exercise and Mod1-17 tutorial

---
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class WordSearch {
+
+	// Use this scanner for all user input. Don't create additional Scanners with System.in
+	private final Scanner userInput = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		WordSearch wordSearch = new WordSearch();
+		wordSearch.run();
+	}
+
+	public void run() {
+		/* Your code goes here */
+
+
+	}
+
+}
