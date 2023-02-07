a71bd9763d8cdc48d6a44b723e1c85ad294c9e1f
Myron Law <myron@techelevator.com>
2023-02-07 11:19:03 -0500

    Added Mod1-17 exercise

---
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class FindAndReplace {
+
+    // Use this scanner for all user input. Don't create additional Scanners with System.in
+    private final Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        FindAndReplace findAndReplace = new FindAndReplace();
+        findAndReplace.run();
+    }
+
+    public void run() {
+        /* Your code goes here */
+
+
+    }
+
+}
