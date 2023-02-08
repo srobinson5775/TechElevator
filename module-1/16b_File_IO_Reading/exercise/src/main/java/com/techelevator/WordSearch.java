deda368318ba8090570fcdf36cbc1a1f04517504
Shaylyn Robinson <srobinson5775@gmail.com>
2023-02-07 20:40:08 -0500

    16b_File_IO_Reading-exercise

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,6 +1,8 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class WordSearch {
@@ -15,12 +17,57 @@ public class WordSearch {
 
 	public void run() {
 		/* Your code goes here */
-		//Shaylyn start here
-		System.out.println("Enter the Name of the file");
-		String filePath = userInput.nextLine();
-		File wordSearchFile = new File(filePath);
+		try (userInput) {
+			File inputFile;
+			while (true) {
+				System.out.println("Enter the file to be searched");
+				String path = userInput.nextLine();
+				inputFile = new File(path);
+				if (!inputFile.exists()) {
+					System.out.println(path + " does not exist");
+					continue;
+				} else if (!inputFile.isFile()) {
+					System.out.println(path + "is not a file");
+					continue;
 
+				}
+
+				//check to make sure that its not null or empty .is empty
+			System.out.println("Enter search word");
+			String searchWord = userInput.nextLine();
+			if ((searchWord == null) || (searchWord.isEmpty())){
+				System.out.println("search word is empty");
+
+			}
+
+			System.out.println("Should the search be case sensitive? (Y/N");
+			boolean caseSensitive = userInput.nextLine().equals("Y");
+
+			int lineNumber = 1;
+
+			try (userInput) {
+				while (userInput.hasNextLine()) {
+					String line = userInput.nextLine();
+					if(!caseSensitive){
+						if(line.toLowerCase().contains(searchWord.toLowerCase())){
+							System.out.println(lineNumber + ")" + line);
+						}
+					}
+					else {
+						if(line.contains(searchWord)){
+							System.out.println(lineNumber + ")" + line);
+						}
+					}
+					lineNumber += 1;
+
+					}
+				}break;
+
+
+			}
+
+
+		}
 
 	}
-
 }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -14,8 +15,19 @@ public class FindAndReplace {
 
     public void run() {
         /* Your code goes here */
+        System.out.println("what is the search word?");
+        String searchWord = userInput.nextLine();
 
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+
+        System.out.println("What is the source file");
+        String pathToSource = userInput.nextLine();
+
+
+        System.out.println("What is the destination file?");
+        String pathToDestination = userInput.nextLine();
 
     }
-
 }
