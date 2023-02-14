6189655b96b1d5647a19ccae3fbd337409a9d555
Myron Law <myron@techelevator.com>
2023-02-14 11:39:07 -0500

    Added Mod2-2 exercise

---
+++ module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
@@ -0,0 +1,4 @@
+-- 20. The state abbreviation, and population of the city with the largest population (name column 'city_population') for all states, territories, and districts.
+-- Order the results from highest to lowest populations.
+-- (56 rows)
+
