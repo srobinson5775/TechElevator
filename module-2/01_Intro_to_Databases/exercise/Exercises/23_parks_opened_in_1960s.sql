f23d1dc361bb8960712ae0e3b286a6817c78c8f4
Shaylyn Robinson <srobinson5775@gmail.com>
2023-02-14 20:59:00 -0500

    Intro_into_databases_exercise

--- module-2/01_Intro_to_Databases/exercise/Exercises/23_parks_opened_in_1960s.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/23_parks_opened_in_1960s.sql
@@ -1,2 +1,4 @@
 -- 23. The name and date established of parks opened in the 1960s (6 rows)
-
+SELECT park_name, date_established
+FROM park
+WHERE date_established > '12/31/1959' AND date_established < '1/1/1970';
